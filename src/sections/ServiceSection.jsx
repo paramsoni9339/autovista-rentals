9b4c66e12fb5276d434321b014c68117790d41ed
paramsoni9339 <paramsoni.nmims@gmail.com>
2025-12-18 03:25:13 +0530

    made cards v1.2

--- src/sections/ServiceSection.jsx
+++ src/sections/ServiceSection.jsx
@@ -1,34 +1,51 @@
 import { Link } from "react-router-dom";
 import { MoveRight } from "lucide-react";
-
+import  ServiceCard  from "./ServiceCard";
+import { featuredServices } from "../assets/assets";
 
 const ServiceSection = () => {
   return (
-    <section className="">
-        <div className="">
+    <section className="section-padding bg-black">
+        <div className="max-w-7xl mx-auto">
 
             {/* Heading  */}
-            <div className="">
+            <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-12">
                 <div>
-                    <h2 className="">
+                    <h2 className="heading-2 clash-display grad1 from-zinc-50">
                         Our Premium Fleet & <br className=""/> Services 
                     </h2>
 
-                    <p className="">
+                    <p className="text-zinc-400 max-w-lg ">
                         Discover our diverse range of luxury vehicles and exceptional services tailored to elevate your driving experience.
                     </p>
                 </div>
 
-                <Link to="/fleet" className="">
-                    View All Cars <MoveRight className=""/>
+                <Link to="/fleet" className="mt-4 sm:mt-0 flex items-center text-sky-500 hover:text-sky-400 transition-colors">
+                    View All Cars <MoveRight className="ml-1 h-4 w-4"/>
 
                 </Link>
 
             </div>
             {/* service cards */}
-            <div className=""></div>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 lg:px-16">
 
-            
+                  {/* service cards */}
+                    {featuredServices.map((service, index) => (
+                        <ServiceCard 
+                        key={index} 
+                        title={service.title} 
+                        description={service.description} 
+                        icon={service.icon} 
+                        imageSrc={service.imageSrc} />
+                        
+                        ))
+                    }
+
+
+
+            </div>
+
+          
 
         </div>
 
